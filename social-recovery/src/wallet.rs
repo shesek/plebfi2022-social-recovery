c0e87be42e18051b2621d9b37fdddb4af63a51df
Nadav Ivgi <nadav@shesek.info>
2022-04-05 05:24:50 -0400

    Add methods for extracting the tweaked pub/priv keys

--- social-recovery/src/wallet.rs
+++ social-recovery/src/wallet.rs
@@ -1,15 +1,15 @@
 use bitcoin::util::bip32::{ExtendedPrivKey, ExtendedPubKey};
+use bitcoin::util::schnorr::{TapTweak, TweakedKeyPair, TweakedPublicKey};
 use bitcoin::util::taproot::TaprootSpendInfo;
-use bitcoin::{secp256k1, Amount, Network};
+use bitcoin::{secp256k1, Address, Amount, Network};
 use minsc::bitcoin;
 
 use minsc::runtime::{Evaluate, Execute};
-use minsc::{parse_lib, Scope};
 
 use crate::backup::{RecoveryParams, UserBackup};
 
 lazy_static! {
-    static ref MINSC_2SRECOVERY_LIB: minsc::ast::Library = parse_lib(
+    static ref MINSC_2SRECOVERY_LIB: minsc::ast::Library = minsc::parse_lib(
         r#"
         fn twoStepRecovery($user_pk, $recovery_pk, $delay, $amount, $fee) =
           $user_pk+(pk($recovery_pk) && txtmpl([
@@ -27,6 +27,7 @@ pub struct UserWallet {
     user_xpriv: ExtendedPrivKey,
     user_xpub: ExtendedPubKey,
     recovery_xpub: ExtendedPubKey,
+    network: Network,
 }
 
 impl UserWallet {
@@ -39,33 +40,64 @@ impl UserWallet {
             user_xpriv,
             user_xpub,
             recovery_xpub: backup.recovery_xpub,
+            network,
         }
     }
 
-    pub fn tapinfo(&self, index: usize, amount: Amount) -> TaprootSpendInfo {
-        let mut scope = Scope::root();
+    pub fn address_pks(&self, index: u32) -> (ExtendedPubKey, ExtendedPubKey) {
+        let user_pk = self.user_xpub.derive_pub(&EC, &[index.into()]).unwrap();
+        let recovery_pk = self.recovery_xpub.derive_pub(&EC, &[index.into()]).unwrap();
+        (user_pk, recovery_pk)
+    }
 
+    pub fn eval_minsc(&self, index: u32, amount: Amount, code: &str) -> minsc::Value {
+        let mut scope = minsc::Scope::root();
         MINSC_2SRECOVERY_LIB.exec(&mut scope).unwrap();
 
-        scope.set("$user_xpub", self.user_xpub).unwrap();
-        scope.set("$recovery_xpub", self.recovery_xpub).unwrap();
+        let (user_pk, recovery_pk) = self.address_pks(index);
+        scope.set("$user_pk", user_pk).unwrap();
+        scope.set("$recovery_pk", recovery_pk).unwrap();
         scope.set("$delay", self.params.delay as i64).unwrap();
         scope.set("$fee", self.params.fee as i64).unwrap();
 
         scope.set("$index", index as i64).unwrap();
         scope.set("$amount", amount.as_sat() as i64).unwrap();
 
-        minsc::parse(
-            "twoStepRecovery($user_xpub/$index, $recovery_xpub/$index, $delay, $amount, $fee)",
+        minsc::parse(code).unwrap().eval(&scope).unwrap()
+    }
+
+    pub fn tapinfo(&self, index: u32, amount: Amount) -> TaprootSpendInfo {
+        self.eval_minsc(
+            index,
+            amount,
+            "twoStepRecovery($user_pk, $recovery_pk, $delay, $amount, $fee)",
         )
-        .unwrap()
-        .eval(&scope)
-        .unwrap()
         .into_tapinfo()
         .unwrap()
     }
 
-    // TODO pub fn output_keypair(&self, index, amount) -> (output_xonlypubkey, output_tweaked_privkey)
+    pub fn tweaked_output_keypair(&self, index: u32, amount: Amount) -> (TweakedKeyPair) {
+        let user_priv = self.user_xpriv.derive_priv(&EC, &[index.into()]).unwrap();
+        let merkle_root = self.tapinfo(index, amount).merkle_root();
+
+        user_priv.to_keypair(&EC).tap_tweak(&EC, merkle_root)
+    }
+
+    pub fn tweaked_output_pubkey(
+        &self,
+        index: u32,
+        amount: Amount,
+    ) -> (TweakedPublicKey, secp256k1::Parity) {
+        let user_pub = self.user_xpub.derive_pub(&EC, &[index.into()]).unwrap();
+        let merkle_root = self.tapinfo(index, amount).merkle_root();
+
+        user_pub.to_x_only_pub().tap_tweak(&EC, merkle_root)
+    }
+
+    pub fn address(&self, index: u32, amount: Amount) -> Address {
+        let (tweaked_pubkey, _) = self.tweaked_output_pubkey(index, amount);
+        Address::p2tr_tweaked(tweaked_pubkey, self.network)
+    }
 }
 
 #[test]
@@ -77,10 +109,18 @@ fn test_wallet() {
         delay: 100,
         fee: 250,
     };
-    let (user_backup, recovery_backup) = create_wallet(params, Network::Signet);
+    let (user_backup, _) = create_wallet(params, Network::Signet);
     let wallet = UserWallet::from_backup(user_backup, Network::Signet);
     let tapinfo = wallet.tapinfo(0, "0.25 BTC".parse().unwrap());
     println!("address 0 tapinfo: {:?}", tapinfo);
+    let keypair = wallet.tweaked_output_keypair(0, "0.25 BTC".parse().unwrap());
+    println!("address 0 keypair: {:?}", keypair);
+    let pubkey = wallet.tweaked_output_pubkey(0, "0.25 BTC".parse().unwrap());
+    println!("address 0 pubkey: {:?}", pubkey);
+    println!(
+        "address 0: {:?}",
+        wallet.address(0, "0.25 BTC".parse().unwrap())
+    );
 }
 
 // slides
